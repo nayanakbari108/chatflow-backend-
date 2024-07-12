c43a859dca1cfec04fd47dfcde641c8493668690
nayanakbari108 <nayanakbari76976@gmail.com>
2024-07-12 14:40:55 +0530

    cors

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const server = app.listen(process.env.PORT, () =>
 );
 const io = socket(server, {
   cors: {
-    origin: "https://chatflow-backend.onrender.com",
+    origin: "https://chatflow-react.vercel.com",
     credentials: true,
   },
 });
